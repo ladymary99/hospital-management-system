a92753444051fe2ec82c9025c6e7798703903b8a
ladymary99 <160885008+ladymary99@users.noreply.github.com>
2024-10-26 20:20:59 +0430

    Update javascript-admin.js

--- javascript/javascript-admin.js
+++ javascript/javascript-admin.js
@@ -1,12 +1,4 @@
-// JavaScript for active links and color change
-document.querySelectorAll(".dashboard-item").forEach((item) => {
-  item.addEventListener("click", function () {
-    document
-      .querySelectorAll(".dashboard-item")
-      .forEach((link) => link.classList.remove("active-item"));
-    this.classList.add("active-item");
-  });
-});
+
 // Available Beds Chart
 // Get the context of the canvas
 const ctx = document.getElementById("availableBedsChart").getContext("2d");
