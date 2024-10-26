2d64455e55dac984fce160ef6cb418833730dd32
ladymary99 <160885008+ladymary99@users.noreply.github.com>
2024-10-26 20:14:52 +0430

    Update settings.js

--- javascript/settings.js
+++ javascript/settings.js
@@ -1,12 +1,3 @@
-// JavaScript for active links and color change
-document.querySelectorAll(".dashboard-item").forEach((item) => {
-  item.addEventListener("click", function () {
-    document
-      .querySelectorAll(".dashboard-item")
-      .forEach((link) => link.classList.remove("active-item"));
-    this.classList.add("active-item");
-  });
-});
 function showContent(tabNumber) {
   // hiding elements
   for (let i = 1; i <= 4; i++) {
@@ -15,4 +6,30 @@ function showContent(tabNumber) {
 
   // showing elements with clicking
   document.getElementById(`content-${tabNumber}`).style.display = "block";
+}
+const changePhotoBtn = document.getElementById("change-photo-btn");
+const photoInput = document.getElementById("photo-input");
+const profilePicture = document.getElementById("profile-picture");
+const sidebarPicture = document.getElementById("sidebar-picture");
+
+changePhotoBtn.addEventListener("click", function () {
+  photoInput.click();
+});
+
+photoInput.addEventListener("change", function (event) {
+  const file = event.target.files[0];
+  if (file) {
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      const newImageSrc = e.target.result;
+
+      updateAllPictures(newImageSrc);
+    };
+    reader.readAsDataURL(file);
+  }
+});
+function updateAllPictures(imageSrc) {
+  profilePicture.src = imageSrc;
+  sidebarPicture.src = imageSrc;
+  
 }
